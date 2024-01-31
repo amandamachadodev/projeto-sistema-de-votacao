e1434d4de2a19568b80814ce53ed02226e955af5
amandamachadodev <machado.amandag@gmail.com>
2024-01-31 11:53:33 -0300

    feat(req 3): implementar subclasse PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,8 +1,25 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Classe PessoaEleitora.
+ * Subclasse PessoaEleitora da superclasse Pessoa.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  /**
+   * Construtor.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }
