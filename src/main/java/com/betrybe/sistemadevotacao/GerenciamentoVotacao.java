d5ee975d653200b01d3672f274690b6a10a2b7c7
amandamachadodev <machado.amandag@gmail.com>
2024-01-31 12:11:18 -0300

    feat(req 4): Implementar atributos da classe gerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,41 @@
 package com.betrybe.sistemadevotacao;
 
-/**
- * Classe GerenciamentoVotacao.
- */
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private final ArrayList<PessoaCandidata> pessoasCandidatas;
+  private final ArrayList<PessoaEleitora> pessoasEleitoras;
+  private final ArrayList<String> cpfsComputados;
+
+  public GerenciamentoVotacao(ArrayList<PessoaCandidata> pessoasCandidatas,
+      ArrayList<PessoaEleitora> pessoasEleitoras, ArrayList<String> cpfsComputados) {
+    this.pessoasCandidatas = pessoasCandidatas;
+    this.pessoasEleitoras = pessoasEleitoras;
+    this.cpfsComputados = cpfsComputados;
+  }
+
+  /**
+   * Construtor.
+   */
+
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
