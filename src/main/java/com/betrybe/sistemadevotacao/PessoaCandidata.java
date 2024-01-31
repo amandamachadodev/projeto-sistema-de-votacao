438f3bc891239ae9ec686fd2869ccfd13459cdbe
amandamachadodev <machado.amandag@gmail.com>
2024-01-31 11:30:23 -0300

    feat(req 2): implementar subclasse PessoaCandidata da superclasse Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Subclasse PessoaCandidata da superclasse Pessoa.
+ */
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  /**
+   * Construtor.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
